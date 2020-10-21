3d4834150047c3fb56469ebb86d4c90864701b50
Dongwook Kim <dev.dionkim@gmail.com>
2020-10-21 13:46:25 +0000

    Learned how to use Packages and Imports

--- 0_MainPackage/main.go
+++ 0_MainPackage/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
   "fmt"
-  "github.com/dionkimm/Go-Study/0_MainPackage/something"
+  "main/0_MainPackage/something"
 )
 
 func main() {
@@ -10,6 +10,7 @@ func main() {
   something.SayHello()
 }
 
+// # 1.0
 
 // Go 언어의 컴파일 실행 파일명은 반드시 main.go이어야 한다.
 // 파일명(main.go의 main)과 package명(package main의 main)이 같아야 한다.
@@ -21,3 +22,8 @@ func main() {
 // go의 컨셉 두가지 
 // 1. package : 파일 맨 위에 니가 작성할 패키지의 이름을 작성하는 곳
 // 2. func : JS나 C랑 비슷
+
+// # 1.1
+
+// 어떤 함수가 export 되기 위해선 그 함수의 첫글자가 대문자여야 한다.
+// something.sayHello 는 되지 않지만, something.SayHello 는 실행됨.
